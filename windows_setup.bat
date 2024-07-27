ae148757a5dfbe9d77a167f3a1df4bf8145b45ed
cjhosken <hoskenchristopher@gmail.com>
2024-07-27 15:37:17 +0100

    windows_setup for labs

--- windows_setup.bat
+++ windows_setup.bat
@@ -10,11 +10,13 @@ set SCRIPT_DIR=%~dp0
 cd %SCRIPT_DIR%
 
 REM Set variables (adjust paths as needed)
-set NCCA_DIR="%USERPROFILE%\.ncca"
-set MAYA_BASE_PATH="%USERPROFILE%\Documents\maya"
+set NCCA_DIR="%HOMESHARE%\.ncca"
+set MAYA_BASE_PATH="%HOMESHARE%\Maya"
 set MAYAPY_BASE_PATH="C:\Program Files\Autodesk"
 set HYTHON_BASE_PATH="C:\Program Files\Side Effects Software"
-set HOUDINI_SHELF_BASE_PATH="%USERPROFILE%\Documents"
+set HOUDINI_SHELF_BASE_PATH="%HOMESHARE%"
+
+echo %NCCA_DIR%
 
 REM Create NCCA_DIR if it doesn't exist
 if not exist "%NCCA_DIR%" (
@@ -32,18 +34,18 @@ xcopy /e /i .\ncca_shelftools "%ncca_shelftools_dir%"
 
 REM Iterate over Maya versions and copy shelf files
 for /d %%d in ("%MAYA_BASE_PATH%\*") do (
-    if exist "%%d\prefs\shelves" (
+    if exist "%%d\Projects\2023\Prefs\shelves" (
         echo Copying to Maya directory: %%d
-        if exist "%%d\prefs\shelves\shelf_NCCA.mel" (
-            del "%%d\prefs\shelves\shelf_NCCA.mel"
+        if exist "%%d\Projects\2023\Prefs\shelves\shelf_NCCA.mel" (
+            del "%%d\Projects\2023\Prefs\shelves\shelf_NCCA.mel"
         )
 
-        copy "%ncca_shelftools_dir%\ncca_for_maya\shelf_NCCA.mel" "%%d\prefs\shelves\shelf_NCCA.mel"
+        copy "%ncca_shelftools_dir%\ncca_for_maya\shelf_NCCA.mel" "%%d\Projects\2023\Prefs\shelves\shelf_NCCA.mel"
     )
 )
 
 REM Iterate over Houdini versions and copy shelf files
-for /d %%d in ("%HOUDINI_SHELF_BASE_PATH%\houdini20.*.*") do (
+for /d %%d in ("%HOUDINI_SHELF_BASE_PATH%\houdini*.*") do (
     if exist "%%d\toolbar" (
         echo Copying to Houdini shelf directory: %%d
         if exist "%%d\toolbar\ncca_hou.shelf" (
@@ -57,8 +59,8 @@ REM Install required Python packages using mayapy
 for /d %%d in (""%MAYAPY_BASE_PATH%\Maya*"") do (
     if exist "%%d\bin\mayapy.exe" (
         echo Installing Requirements for mayapy: %%d
-        "%%d\bin\mayapy.exe" -m pip install --upgrade pip
-        "%%d\bin\mayapy.exe" -m pip install -r requirements.txt
+        "%%d\bin\mayapy.exe" -m pip install --upgrade pip --user
+        "%%d\bin\mayapy.exe" -m pip install -r requirements.txt --user
     )
 )
 
@@ -66,8 +68,8 @@ REM Install required Python packages using hython
 for /d %%d in (""%HYTHON_BASE_PATH%\Houdini*"") do (
     if exist "%%d\bin\hython.exe" (
         echo Installing Requirements for hython: %%d
-        "%%d\bin\hython.exe" -m pip install --upgrade pip
-        "%%d\bin\hython.exe" -m pip install -r requirements.txt
+        "%%d\bin\hython.exe" -m pip install --upgrade pip --user
+        "%%d\bin\hython.exe" -m pip install -r requirements.txt --user
     )
 )
 
