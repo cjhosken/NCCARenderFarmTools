f352305635102860ca1a9278de64adf0700e771d
cjhosken <hoskenchristopher@gmail.com>
2024-08-23 23:25:43 +0100

    karma and arnold test files

--- exr_channels.py
+++ exr_channels.py
@@ -2,10 +2,10 @@ import OpenEXR
 import Imath
 
 # Open the EXR file
-file_path = 'flip.exr'
+file_path = 'karma.exr'
 exr_file = OpenEXR.InputFile(file_path)
 
-# Get the header data
+# Get the header data``
 header_data = exr_file.header()
 
 # Print the header data
